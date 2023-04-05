7f13e6b2c57969af23ce93418dc0cc97a6e6cf86
karrmao <karrmao@gmail.com>
2023-04-05 11:43:56 +0300

    fixed search again

--- src/flights/components/tab/Table.jsx
+++ src/flights/components/tab/Table.jsx
@@ -32,9 +32,7 @@ const Table = ({ flightsData, dateValue }) => {
   const filtredFlight =
     searchInputValue === null
       ? currentFlight
-      : currentFlight.filter(fly =>
-          fly[nameArrow].toLowerCase().includes(searchInputValue.toLowerCase()),
-        );
+      : currentFlight.filter(fly => fly[nameArrow].includes(searchInputValue));
 
   console.log('data', filtredFlight);
 
