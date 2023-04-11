01335aa1010156e930f657b873ac035fa1c7b7f6
karrmao <karrmao@gmail.com>
2023-04-11 11:22:29 +0300

    add fix

--- src/flights/components/btnNearestDay/BtnNearestDay.jsx
+++ src/flights/components/btnNearestDay/BtnNearestDay.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import moment from 'moment';
+import { getFullYear } from '../../dateUtils';
+
 import '../date_picker/datePicker.scss';
 
 const BtnNearestDay = ({ text, date, onClick, dateValue }) => (
-  <button
-    className={date === moment(dateValue).format('DD/MM') && 'nearest-day--active'}
-    onClick={onClick}
-  >
+  <button className={date === getFullYear(dateValue) && 'nearest-day--active'} onClick={onClick}>
     {date}
     <span>{text}</span>
   </button>
 );
 export default BtnNearestDay;
-
-//
